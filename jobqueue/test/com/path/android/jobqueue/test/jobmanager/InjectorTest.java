5cf2c7e87b61e3dffb4ff2e3a3f2f187699b58e1
yigit <yboyar@gmail.com>
2014-02-18 17:28:59 -0800

    failing unit test fest exceptions in background thread

--- jobqueue/test/com/path/android/jobqueue/test/jobmanager/InjectorTest.java
+++ jobqueue/test/com/path/android/jobqueue/test/jobmanager/InjectorTest.java
@@ -6,6 +6,7 @@ import com.path.android.jobqueue.JobManager;
 import com.path.android.jobqueue.Params;
 import com.path.android.jobqueue.config.Configuration;
 import com.path.android.jobqueue.di.DependencyInjector;
+import com.path.android.jobqueue.log.CustomLogger;
 import com.path.android.jobqueue.test.jobs.DummyJob;
 import static org.hamcrest.CoreMatchers.*;
 import org.hamcrest.*;
@@ -13,6 +14,8 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.robolectric.*;
 
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.TimeUnit;
 import java.util.concurrent.atomic.AtomicInteger;
 
 @RunWith(RobolectricTestRunner.class)
@@ -42,6 +45,55 @@ public class InjectorTest extends JobManagerTestBase {
         holder = getNextJobMethod(jobManager).invoke();
         MatcherAssert.assertThat("injection should be called for persistent job", holder.getBaseJob(), equalTo(injectedJobReference.getObject()));
         MatcherAssert.assertThat("injection should be called two times for persistent job", injectionCallCount.get(), equalTo(3));
+    }
+
+    @Test
+    public void testInjectorCrash() throws Exception {
+        final String EXCEPTION_MESSAGE = "could not inject for whatever reason :)";
+        DependencyInjector dummyDependencyInjector = new DependencyInjector() {
+            @Override
+            public void inject(BaseJob baseJob) {
+                throw new RuntimeException(EXCEPTION_MESSAGE);
+            }
+        };
+
+        final ObjectReference objectReference = new ObjectReference();
+        final CountDownLatch exceptionLatch = new CountDownLatch(1);
+        CustomLogger customLogger = new CustomLogger() {
+            @Override
+            public boolean isDebugEnabled() {
+                return false;
+            }
+
+            @Override
+            public void d(String s, Object... objects) {
+
+            }
+
+            @Override
+            public void e(Throwable throwable, String s, Object... objects) {
+                objectReference.setObject(throwable);
+                exceptionLatch.countDown();
+            }
+
+            @Override
+            public void e(String s, Object... objects) {
+                //
+            }
+        };
+        JobManager jobManager = createJobManager(new Configuration.Builder(Robolectric.application).injector(dummyDependencyInjector));
+        Throwable addException = null;
+        try {
+            jobManager.addJob(new DummyJob(new Params(0)));
+        } catch (Throwable t) {
+            addException = t;
+        }
+        MatcherAssert.assertThat("addJob should throw exception if dependency injector throws exception", addException, notNullValue());
+        jobManager.addJobInBackground(new DummyJob(new Params(0)));
+        exceptionLatch.await(2, TimeUnit.SECONDS);
+        MatcherAssert.assertThat("there should be a received exception", objectReference.getObject(), notNullValue());
+        MatcherAssert.assertThat("logged exception should be a runtime exception", objectReference.getObject(), instanceOf(RuntimeException.class));
+        MatcherAssert.assertThat("logged exception should have expected message", ((Throwable)objectReference.getObject()).getMessage(), is(EXCEPTION_MESSAGE));
 
     }
 }
