7b69ac75e2bc0b426714b574a1080fffcac50cb3
Coltin Caverhill <coltin@yelp.com>
2015-11-03 10:52:30 -0500

    Fixed lint errors for api versions.

--- jobqueue/src/main/java/com/path/android/jobqueue/network/NetworkUtilImpl.java
+++ jobqueue/src/main/java/com/path/android/jobqueue/network/NetworkUtilImpl.java
@@ -1,5 +1,6 @@
 package com.path.android.jobqueue.network;
 
+import android.annotation.TargetApi;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
@@ -7,7 +8,6 @@ import android.content.IntentFilter;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
 import android.os.Build.VERSION;
-import android.os.Build.VERSION_CODES;
 import android.os.PowerManager;
 
 /**
@@ -16,10 +16,6 @@ import android.os.PowerManager;
 public class NetworkUtilImpl implements NetworkUtil, NetworkEventProvider {
     private Listener listener;
     public NetworkUtilImpl(Context context) {
-        IntentFilter networkIntentFilter = new IntentFilter(ConnectivityManager.CONNECTIVITY_ACTION);
-        if (VERSION.SDK_INT >= VERSION_CODES.M) {
-            networkIntentFilter.addAction(PowerManager.ACTION_DEVICE_IDLE_MODE_CHANGED);
-        }
         context.getApplicationContext().registerReceiver(new BroadcastReceiver() {
             @Override
             public void onReceive(Context context, Intent intent) {
@@ -31,24 +27,41 @@ public class NetworkUtilImpl implements NetworkUtil, NetworkEventProvider {
                 // through getActiveNetworkInfo() or getAllNetworkInfo().
                 listener.onNetworkChange(isConnected(context));
             }
-        }, networkIntentFilter);
+        }, getNetworkIntentFilter());
     }
 
     @Override
     public boolean isConnected(Context context) {
-        // During Doze mode, also called Idle, the network is unavailable but isConnectedOrConnecting()
-        // will return true. So we first check if we are in idle mode through the PowerManager before
-        // trusting the ConnectivityManager.
-        if (VERSION.SDK_INT >= VERSION_CODES.M) {
-            PowerManager powerManager = (PowerManager) context.getSystemService(Context.POWER_SERVICE);
-            if (powerManager.isDeviceIdleMode()) {
-                return false;
-            }
+        if (isDozing(context)) {
+            return false;
         }
 
         ConnectivityManager cm = (ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE);
         NetworkInfo netInfo = cm.getActiveNetworkInfo();
         return netInfo != null && netInfo.isConnectedOrConnecting();
+    }
+
+    @TargetApi(23)
+    private static IntentFilter getNetworkIntentFilter() {
+        IntentFilter networkIntentFilter = new IntentFilter(ConnectivityManager.CONNECTIVITY_ACTION);
+        if (VERSION.SDK_INT >= 23) {
+            networkIntentFilter.addAction(PowerManager.ACTION_DEVICE_IDLE_MODE_CHANGED);
+        }
+        return networkIntentFilter;
+    }
+
+    /**
+     * Returns true if the device is in Doze/Idle mode. Should be called before checking the network connection because
+     * the ConnectionManager may report the device is connected when it isn't during Idle mode.
+     */
+    @TargetApi(23)
+    private static boolean isDozing(Context context) {
+        if (VERSION.SDK_INT >= 23) {
+            PowerManager powerManager = (PowerManager) context.getSystemService(Context.POWER_SERVICE);
+            return powerManager.isDeviceIdleMode();
+        } else {
+            return false;
+        }
     }
 
     @Override
