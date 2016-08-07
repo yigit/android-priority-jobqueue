70334ae3671968f87113ec982ac031cd5ccc1840
Yigit Boyar <yboyar@gmail.com>
2016-08-07 11:01:29 -0700

    More Scheduler Tests
    * FrameworkSchedulerService tests
    
    * Gcm scheduler tests
    
    * removed unused constructor in CancelResult

--- jobqueue/src/test/java/com/birbit/android/jobqueue/scheduling/GcmSchedulerTest.java
+++ jobqueue/src/test/java/com/birbit/android/jobqueue/scheduling/GcmSchedulerTest.java
@@ -1,27 +1,31 @@
 package com.birbit.android.jobqueue.scheduling;
 
 import android.annotation.TargetApi;
-import android.app.job.JobInfo;
-import android.app.job.JobParameters;
-import android.app.job.JobScheduler;
-import android.content.ComponentName;
 import android.content.Context;
 import android.content.SharedPreferences;
 import android.os.Build;
-import android.os.PersistableBundle;
-import android.support.annotation.NonNull;
+import android.os.Bundle;
 
 import com.birbit.android.jobqueue.network.NetworkUtil;
+import com.google.android.gms.gcm.GcmNetworkManager;
+import com.google.android.gms.gcm.OneoffTask;
+import com.google.android.gms.gcm.Task;
+import com.google.android.gms.gcm.TaskParams;
 
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.ArgumentCaptor;
 import org.mockito.Mockito;
+import org.mockito.invocation.InvocationOnMock;
+import org.mockito.stubbing.Answer;
 import org.robolectric.RobolectricGradleTestRunner;
+import org.robolectric.RuntimeEnvironment;
 import org.robolectric.annotation.Config;
 
-import static org.hamcrest.CoreMatchers.equalTo;
+import java.util.UUID;
+import java.util.concurrent.TimeUnit;
+
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.CoreMatchers.nullValue;
 import static org.hamcrest.MatcherAssert.assertThat;
@@ -34,19 +38,19 @@ import static org.mockito.Mockito.when;
 @RunWith(RobolectricGradleTestRunner.class)
 @Config(constants = com.birbit.android.jobqueue.BuildConfig.class)
 @TargetApi(Build.VERSION_CODES.LOLLIPOP)
-public class FrameworkSchedulerTest {
-    FrameworkScheduler fwScheduler;
+public class GcmSchedulerTest {
+    GcmScheduler gcmScheduler;
+    GcmNetworkManager mockGcmNetworkManager;
     Context mockContext;
     Scheduler.Callback mockCallback;
-    JobScheduler mockJobScheduler;
     SharedPreferences mockSharedPreferences;
     SharedPreferences.Editor mockEditor;
 
     @Before
     public void init() {
-        fwScheduler = new FrameworkScheduler(MockFwService.class);
-
-        mockJobScheduler = mock(JobScheduler.class);
+        gcmScheduler = new GcmScheduler(RuntimeEnvironment.application, MockGcmService.class);
+        mockGcmNetworkManager = mock(GcmNetworkManager.class);
+        gcmScheduler.gcmNetworkManager = mockGcmNetworkManager;
 
         mockContext = mock(Context.class);
         mockEditor = mock(SharedPreferences.Editor.class);
@@ -58,71 +62,68 @@ public class FrameworkSchedulerTest {
 
         when(mockContext.getApplicationContext()).thenReturn(mockContext);
         when(mockContext.getPackageName()).thenReturn("com.foo");
-        when(mockContext.getSystemService(Context.JOB_SCHEDULER_SERVICE))
-                .thenReturn(mockJobScheduler);
         when(mockContext.getSharedPreferences(anyString(), anyInt()))
                 .thenReturn(mockSharedPreferences);
 
         mockCallback = mock(Scheduler.Callback.class);
-        fwScheduler.init(mockContext, mockCallback);
-    }
-
-    @Test
-    public void componentName() {
-        assertThat(fwScheduler.getComponentName(),
-                equalTo(new ComponentName(mockContext, MockFwService.class)));
+        gcmScheduler.init(mockContext, mockCallback);
     }
 
     @Test
     public void requestSimple() {
-        networkTest(NetworkUtil.METERED, JobInfo.NETWORK_TYPE_ANY);
+        networkTest(NetworkUtil.METERED, Task.NETWORK_STATE_CONNECTED);
     }
 
     @Test
     public void requestUnmetered() {
-        networkTest(NetworkUtil.UNMETERED, JobInfo.NETWORK_TYPE_UNMETERED);
+        networkTest(NetworkUtil.UNMETERED, Task.NETWORK_STATE_UNMETERED);
     }
 
     @Test
     public void requestAnyNetwork() {
-        networkTest(NetworkUtil.DISCONNECTED, JobInfo.NETWORK_TYPE_NONE);
+        networkTest(NetworkUtil.DISCONNECTED, Task.NETWORK_STATE_ANY);
     }
 
     @Test
     public void delay() {
+        long delaySeconds = 3;
         SchedulerConstraint constraint = mock(SchedulerConstraint.class);
         when(constraint.getNetworkStatus()).thenReturn(NetworkUtil.DISCONNECTED);
-        when(constraint.getDelayInMs()).thenReturn(133L);
-        JobInfo jobInfo = schedule(constraint);
+        when(constraint.getDelayInMs()).thenReturn(TimeUnit.SECONDS.toMillis(delaySeconds));
+        when(constraint.getOverrideDeadlineInMs()).thenReturn(null);
+        OneoffTask jobInfo = schedule(constraint);
         assertThat(jobInfo.isPersisted(), is(true));
-        assertThat(jobInfo.getId(), is(1));
-        assertThat(jobInfo.getMinLatencyMillis(), is(133L));
-        assertThat(jobInfo.getMaxExecutionDelayMillis(), is(0L));
+        assertThat(jobInfo.getWindowStart(), is(delaySeconds));
+        assertThat(jobInfo.getWindowEnd(), is(delaySeconds +
+                gcmScheduler.getExecutionWindowSizeInSeconds()));
     }
 
     @Test
     public void deadline() {
         SchedulerConstraint constraint = mock(SchedulerConstraint.class);
         when(constraint.getNetworkStatus()).thenReturn(NetworkUtil.DISCONNECTED);
-        when(constraint.getOverrideDeadlineInMs()).thenReturn(255L);
-        JobInfo jobInfo = schedule(constraint);
+        when(constraint.getOverrideDeadlineInMs()).thenReturn(
+                TimeUnit.SECONDS.toMillis(37)
+        );
+        OneoffTask jobInfo = schedule(constraint);
         assertThat(jobInfo.isPersisted(), is(true));
-        assertThat(jobInfo.getId(), is(1));
-        assertThat(jobInfo.getMinLatencyMillis(), is(0L));
-        assertThat(jobInfo.getMaxExecutionDelayMillis(), is(255L));
+        assertThat(jobInfo.getWindowStart(), is(0L));
+        assertThat(jobInfo.getWindowEnd(), is(37L));
     }
 
     @Test
     public void deadlineAndDelay() {
+        long delaySeconds = 43;
+        long deadlineSeconds = 57;
         SchedulerConstraint constraint = mock(SchedulerConstraint.class);
         when(constraint.getNetworkStatus()).thenReturn(NetworkUtil.DISCONNECTED);
-        when(constraint.getOverrideDeadlineInMs()).thenReturn(255L);
-        when(constraint.getDelayInMs()).thenReturn(133L);
-        JobInfo jobInfo = schedule(constraint);
+        when(constraint.getOverrideDeadlineInMs()).thenReturn(
+                TimeUnit.SECONDS.toMillis(deadlineSeconds));
+        when(constraint.getDelayInMs()).thenReturn(TimeUnit.SECONDS.toMillis(delaySeconds));
+        OneoffTask jobInfo = schedule(constraint);
         assertThat(jobInfo.isPersisted(), is(true));
-        assertThat(jobInfo.getId(), is(1));
-        assertThat(jobInfo.getMinLatencyMillis(), is(133L));
-        assertThat(jobInfo.getMaxExecutionDelayMillis(), is(255L));
+        assertThat(jobInfo.getWindowStart(), is(delaySeconds));
+        assertThat(jobInfo.getWindowEnd(), is(deadlineSeconds));
     }
 
     @Test
@@ -132,9 +133,9 @@ public class FrameworkSchedulerTest {
         when(constraint.getUuid()).thenReturn("abc");
         when(constraint.getDelayInMs()).thenReturn(345L);
         when(constraint.getOverrideDeadlineInMs()).thenReturn(235L);
-        JobInfo jobInfo = schedule(constraint);
-        PersistableBundle extras = jobInfo.getExtras();
-        SchedulerConstraint fromBundle = FrameworkScheduler.fromBundle(extras);
+        OneoffTask jobInfo = schedule(constraint);
+        Bundle extras = jobInfo.getExtras();
+        SchedulerConstraint fromBundle = GcmScheduler.fromBundle(extras);
         assertThat(fromBundle.getNetworkStatus(), is(NetworkUtil.METERED));
         assertThat(fromBundle.getUuid(), is("abc"));
         assertThat(fromBundle.getDelayInMs(), is(345L));
@@ -148,9 +149,9 @@ public class FrameworkSchedulerTest {
         when(constraint.getUuid()).thenReturn("abc");
         when(constraint.getDelayInMs()).thenReturn(345L);
         when(constraint.getOverrideDeadlineInMs()).thenReturn(null);
-        JobInfo jobInfo = schedule(constraint);
-        PersistableBundle extras = jobInfo.getExtras();
-        SchedulerConstraint fromBundle = FrameworkScheduler.fromBundle(extras);
+        OneoffTask jobInfo = schedule(constraint);
+        Bundle extras = jobInfo.getExtras();
+        SchedulerConstraint fromBundle = GcmScheduler.fromBundle(extras);
         assertThat(fromBundle.getNetworkStatus(), is(NetworkUtil.METERED));
         assertThat(fromBundle.getUuid(), is("abc"));
         assertThat(fromBundle.getDelayInMs(), is(345L));
@@ -192,106 +193,74 @@ public class FrameworkSchedulerTest {
         onStartTest(NetworkUtil.UNMETERED, 35, 77L);
     }
 
-    @Test
-    public void onFinished() {
-        SchedulerConstraint constraint = mock(SchedulerConstraint.class);
-        when(constraint.getNetworkStatus()).thenReturn(NetworkUtil.METERED);
-        when(constraint.getUuid()).thenReturn("abc");
-        when(constraint.getDelayInMs()).thenReturn(22L);
-        when(constraint.getOverrideDeadlineInMs()).thenReturn(null);
-        JobParameters[] outParams = new JobParameters[1];
-        SchedulerConstraint received = triggerOnStart(constraint, outParams);
-        fwScheduler.onFinished(received, false);
-        // TODO would be nice to use powermock and assert onFinished call
-    }
-
-    @Test
-    public void cancelAll() {
-        fwScheduler.cancelAll();
-        verify(mockJobScheduler).cancelAll();
-    }
-
     private void onStartTest(int networkStatus, long delay, Long overrideDeadline) {
         SchedulerConstraint constraint = mock(SchedulerConstraint.class);
         when(constraint.getNetworkStatus()).thenReturn(networkStatus);
         when(constraint.getUuid()).thenReturn("abc");
         when(constraint.getDelayInMs()).thenReturn(delay);
         when(constraint.getOverrideDeadlineInMs()).thenReturn(overrideDeadline);
-        JobParameters[] outParams = new JobParameters[1];
-        SchedulerConstraint received = triggerOnStart(constraint, outParams);
+        SchedulerConstraint received = triggerOnStart(constraint);
 
-        assertThat(received.getOverrideDeadlineInMs(), is(overrideDeadline));
-        assertThat(received.getDelayInMs(), is(delay));
-        assertThat(received.getUuid(), is("abc"));
-        assertThat(received.getNetworkStatus(), is(networkStatus));
-
-        received = triggerOnStop(constraint, outParams[0]);
         assertThat(received.getOverrideDeadlineInMs(), is(overrideDeadline));
         assertThat(received.getDelayInMs(), is(delay));
         assertThat(received.getUuid(), is("abc"));
         assertThat(received.getNetworkStatus(), is(networkStatus));
     }
 
-    private SchedulerConstraint triggerOnStart(SchedulerConstraint constraint, JobParameters[]
-                                               outParams) {
+    private SchedulerConstraint triggerOnStart(SchedulerConstraint constraint) {
         ArgumentCaptor<SchedulerConstraint> constraintCaptor =
                 ArgumentCaptor.forClass(SchedulerConstraint.class);
-        when(mockCallback.start(constraintCaptor.capture())).thenReturn(true);
+        when(mockCallback.start(constraintCaptor.capture())).thenAnswer(new Answer<Boolean>() {
+            @Override
+            public Boolean answer(final InvocationOnMock invocation) throws Throwable {
+                new Thread(new Runnable() {
+                    @Override
+                    public void run() {
+                        SchedulerConstraint constrant =
+                                (SchedulerConstraint) invocation.getArguments()[0];
+                        gcmScheduler.onFinished(constrant, false);
+                    }
+                }).start();
+                return true;
+            }
+        });
 
-        JobParameters params = prepareJobParameters(constraint);
-        outParams[0] = params;
-        fwScheduler.onStartJob(params);
+        TaskParams params = prepareJobParameters(constraint);
+        gcmScheduler.onStartJob(params);
 
         verify(mockCallback).start(Mockito.any(SchedulerConstraint.class));
         return constraintCaptor.getValue();
     }
 
-    private SchedulerConstraint triggerOnStop(SchedulerConstraint constraint,
-                                              JobParameters params) {
-        ArgumentCaptor<SchedulerConstraint> constraintCaptor =
-                ArgumentCaptor.forClass(SchedulerConstraint.class);
-        when(mockCallback.stop(constraintCaptor.capture())).thenReturn(false);
-
-        fwScheduler.onStopJob(params);
-
-        verify(mockCallback).stop(Mockito.any(SchedulerConstraint.class));
-        return constraintCaptor.getValue();
+    private TaskParams prepareJobParameters(SchedulerConstraint constraint) {
+        return new TaskParams(constraint.getUuid(),
+                GcmScheduler.toBundle(constraint));
     }
 
-    @NonNull
-    private JobParameters prepareJobParameters(SchedulerConstraint constraint) {
-        JobInfo info = schedule(constraint);
-
-        JobParameters params = mock(JobParameters.class);
-        when(params.getExtras()).thenReturn(info.getExtras());
-        when(params.getJobId()).thenReturn(info.getId());
-        return params;
-    }
 
     private void networkTest(@NetworkUtil.NetworkStatus int networkType, int expected) {
         SchedulerConstraint constraint = mock(SchedulerConstraint.class);
         when(constraint.getNetworkStatus()).thenReturn(networkType);
-        JobInfo jobInfo = schedule(constraint);
+        when(constraint.getOverrideDeadlineInMs()).thenReturn(null);
+
+        OneoffTask jobInfo = schedule(constraint);
         assertThat(jobInfo.isPersisted(), is(true));
-        assertThat(jobInfo.getId(), is(1));
-        assertThat(jobInfo.getMinLatencyMillis(), is(0L));
-        assertThat(jobInfo.getMaxExecutionDelayMillis(), is(0L));
-        assertThat(jobInfo.getNetworkType(), is(expected));
+        assertThat(jobInfo.getRequiredNetwork(), is(expected));
+        assertThat(jobInfo.getWindowStart(), is(0L));
+
+        assertThat(jobInfo.getWindowEnd(), is(gcmScheduler.getExecutionWindowSizeInSeconds()));
     }
 
-    @Test
-    public void createId() {
-        when(mockSharedPreferences.getInt(FrameworkScheduler.KEY_ID, 0)).thenReturn(33);
-        assertThat(fwScheduler.createId(), is(34));
-        verify(mockEditor).putInt(FrameworkScheduler.KEY_ID, 34);
+    private OneoffTask schedule(SchedulerConstraint constraint) {
+        if (constraint.getUuid() == null) {
+            when(constraint.getUuid()).thenReturn(UUID.randomUUID().toString());
+        }
+        ArgumentCaptor<OneoffTask> infoCaptor = ArgumentCaptor.forClass(OneoffTask.class);
+        gcmScheduler.request(constraint);
+        verify(mockGcmNetworkManager).schedule(infoCaptor.capture());
+        OneoffTask jobInfo = infoCaptor.getValue();
+        assertThat(jobInfo.getTag(), is(constraint.getUuid()));
+        return jobInfo;
     }
 
-    private JobInfo schedule(SchedulerConstraint constraint) {
-        ArgumentCaptor<JobInfo> infoCaptor = ArgumentCaptor.forClass(JobInfo.class);
-        when(mockJobScheduler.schedule(infoCaptor.capture())).thenReturn(1);
-        fwScheduler.request(constraint);
-        verify(mockJobScheduler).schedule(Mockito.any(JobInfo.class));
-        verify(mockEditor).putInt(FrameworkScheduler.KEY_ID, 1);
-        return infoCaptor.getValue();
-    }
 }
